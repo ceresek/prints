37b53caa5658183b409467c589f32b559f58780a
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2024-01-21 19:44:32 +0100

    Added Ikea IVAR holder for Tassimo packages

---
+++ things/ivar-holder-tassimo/README.md
@@ -0,0 +1,16 @@
+# About
+
+A simple clip to hang Tassimo capsule packages on a shelf of the Ikea IVAR system.
+The single sided version is for hanging the packages below a shelf,
+the double sided version is on the photo.
+
+Should fit any shelf with the same height.
+
+# Printing
+
+Printed in PLA with 0.2 mm layers, 4 perimeters for strength.
+
+# Copying
+
+Model CC-BY ceres@tmatmouci.cz.
+Using SolidPython and OpenSCAD.
---
+++ things/ivar-holder-tassimo/main.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+import math as _math
+
+from solid2 import *
+
+from helpers.ivar import *
+
+
+set_global_fn (33)
+
+
+OVERLAP = 0.001
+
+CLASP_WIDTH = 8
+CLASP_THICKNESS = 3.3
+
+HOOK_EDGE_DISTANCE = 10
+HOOK_HOOK_DISTANCE = 53
+EDGE_EDGE_DISTANCE = HOOK_EDGE_DISTANCE + HOOK_HOOK_DISTANCE + HOOK_EDGE_DISTANCE
+
+HOOK_BASE_RADIUS = 10 / 2
+HOOK_WIDE_RADIUS = HOOK_BASE_RADIUS + 0.33
+HOOK_TIPS_RADIUS = 3.33
+
+HOOK_LENGTH_BOTTOM = 2
+HOOK_LENGTH_MIDDLE = 1
+HOOK_LENGTH_TOP = 2
+HOOK_LENGTH = HOOK_LENGTH_BOTTOM + HOOK_LENGTH_MIDDLE + HOOK_LENGTH_TOP
+
+CATCH_BEND = 0.666
+CATCH_SPACE = 0.666
+CATCH_LENGTH = 10
+CATCH_DISTANCE = EDGE_EDGE_DISTANCE / 2 - CATCH_LENGTH / 2
+
+
+def hook ():
+
+    hook = (
+        cylinder (HOOK_LENGTH_BOTTOM, r1 = HOOK_BASE_RADIUS, r2 = HOOK_WIDE_RADIUS) +
+        cylinder (HOOK_LENGTH_MIDDLE, r = HOOK_WIDE_RADIUS).up (HOOK_LENGTH_BOTTOM) +
+        cylinder (HOOK_LENGTH_TOP, r1 = HOOK_WIDE_RADIUS, r2 = HOOK_TIPS_RADIUS).up (HOOK_LENGTH_BOTTOM + HOOK_LENGTH_MIDDLE)
+    ) * (
+        cube (HOOK_WIDE_RADIUS * 2, CLASP_WIDTH, HOOK_LENGTH)
+            .left (HOOK_WIDE_RADIUS)
+            .back (CLASP_WIDTH / 2)
+    )
+
+    return hook
+
+
+def clasp_base ():
+
+    catch_slope_cross_height = CATCH_BEND + CATCH_SPACE + CLASP_THICKNESS
+
+    catch_slope_cross_length = _math.sqrt (catch_slope_cross_height**2 + CATCH_DISTANCE**2)
+    catch_slope_real_length = _math.sqrt (catch_slope_cross_length**2 - CLASP_THICKNESS**2)
+
+    catch_slope_cross_angle = _math.asin (catch_slope_cross_height / catch_slope_cross_length)
+    catch_slope_delta_angle = _math.asin (CLASP_THICKNESS / catch_slope_cross_length)
+    catch_slope_real_angle = catch_slope_cross_angle - catch_slope_delta_angle
+
+    clasp = (
+        (
+            cube (CLASP_THICKNESS + EDGE_EDGE_DISTANCE, CLASP_WIDTH, CLASP_THICKNESS)
+                .left (CLASP_THICKNESS)
+                .down (CLASP_THICKNESS) +
+            cube (CLASP_THICKNESS, CLASP_WIDTH, CLASP_THICKNESS + SHELF_HEIGHT + CATCH_SPACE + CLASP_THICKNESS)
+                .left (CLASP_THICKNESS)
+                .down (CLASP_THICKNESS) +
+            cube (catch_slope_real_length, CLASP_WIDTH, CLASP_THICKNESS)
+                .down (CLASP_THICKNESS)
+                .rotate (0, _math.degrees (catch_slope_real_angle), 0)
+                .up (CLASP_THICKNESS + SHELF_HEIGHT + CATCH_SPACE)
+        )
+        .back (CLASP_WIDTH / 2)
+    )
+
+    hooks = (
+        (
+            hook ().right (HOOK_EDGE_DISTANCE) +
+            hook ().right (HOOK_EDGE_DISTANCE + HOOK_HOOK_DISTANCE)
+        )
+        .rotate (180, 0, 0)
+        .down (CLASP_THICKNESS)
+    )
+
+    return clasp + hooks
+
+
+def holder_single ():
+
+    holder = (
+        clasp_base () +
+        cube (CATCH_LENGTH, CLASP_WIDTH, CLASP_THICKNESS)
+            .right (CATCH_DISTANCE)
+            .up (SHELF_HEIGHT - CATCH_BEND)
+            .back (CLASP_WIDTH/2)
+    )
+
+    return holder
+
+
+def holder_double ():
+
+    holder = (
+        clasp_base () +
+        cube (EDGE_EDGE_DISTANCE - CATCH_DISTANCE, CLASP_WIDTH, CLASP_THICKNESS)
+            .right (CATCH_DISTANCE)
+            .up (SHELF_HEIGHT - CATCH_BEND)
+            .back (CLASP_WIDTH/2) +
+        hook ()
+            .right (EDGE_EDGE_DISTANCE - HOOK_EDGE_DISTANCE)
+            .up (SHELF_HEIGHT + CLASP_THICKNESS - CATCH_BEND)
+    )
+
+    return holder
+
+
+# Main
+
+holder_single ().save_as_scad ('holder_single.scad')
+holder_double ().save_as_scad ('holder_double.scad')
